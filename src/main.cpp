952cb0198a113e826bef476d6144ceaa5b88f42a
U-DESKTOP-J0QPH5V\Паха <pasha_w790@bk.ru>
2025-07-02 05:06:15 +0700

    upgrate Добавлена интеграция PDCurses и настройка проекта 1.Установлена и собрана библиотека PDCurses 2.Создана структура папок для уровней игры 3.Настроен CMake для поддержки curses 4.Добавлен минимальный рабочий пример с инициализацией curses 5.Подготовка к разработке игры

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "Game.hpp"
+#include <curses.h>
 
 int main()
 {
     Game game;
     game.run();
+    initscr();          // Инициализация экрана curses
+    printw("Hello!");   // Вывод текста
+    refresh();          // Обновление экрана
+    getch();            // Ожидание нажатия клавиши
+    endwin();           // Завершение работы с curses
+    
     return 0;
 }
